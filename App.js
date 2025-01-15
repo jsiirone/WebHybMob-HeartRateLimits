05cf681bf46ac9d5e90ba746422ac28d904caf5f
jsiirone <joonas.siironen@gmail.com>
2025-01-15 18:18:47 +0200

    done

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [age, setAge] = useState('')
+  const [lowerLimit, setLowerLimit] = useState(0)
+  const [upperLimit, setUpperLimit] = useState(0)
+
+  const calculate = () => {
+    setLowerLimit((220-age)*0.65)
+    setUpperLimit((220-age)*0.85)
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.field}>Age</Text>
+      <TextInput
+        style={styles.field}
+        value={age}
+        onChangeText={text => setAge(text)}
+        keyboardType='decimal-pad'
+      />
+      <Text style={styles.field}>Limits</Text>
+      <Text style={styles.field}>{lowerLimit.toFixed(0)}-{upperLimit.toFixed(0)}</Text>
+      <Button title="Calculate" onPress={calculate}></Button>
     </View>
   );
 }
@@ -13,8 +32,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 50,
+    marginLeft: 10
   },
+  field: {
+    marginBottom: 10
+  }
 });
